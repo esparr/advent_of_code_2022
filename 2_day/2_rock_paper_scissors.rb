34ce4b13aed6524d1251f9b7622144543a23e0a7
Em Sparr <em.c.sparr@gmail.com>
2022-12-05 12:04:32 -0500

    chore: cleans up linting

--- 2_day/2_rock_paper_scissors.rb
+++ 2_day/2_rock_paper_scissors.rb
@@ -3,18 +3,18 @@
 # The Rock Paper Scissors class returns total score for elf cheat sheet.
 class RockPaperScissors
   TIE_POINT_KEY = [
-    { match: %w[A X], score: 1 },
-    { match: %w[B Y], score: 2 },
-    { match: %w[C Z], score: 3 }
+    { match: %w[A X], points: 1 },
+    { match: %w[B Y], points: 2 },
+    { match: %w[C Z], points: 3 }
   ].freeze
 
   POINT_KEY = [
-    { match: %w[A Z], w_score: 1, winner: 'A', l_score: 3, loser: 'Z' },
-    { match: %w[C X], w_score: 1, winner: 'X', l_score: 3, loser: 'C' },
-    { match: %w[B X], w_score: 2, winner: 'B', l_score: 1, loser: 'X' },
-    { match: %w[A Y], w_score: 2, winner: 'Y', l_score: 1, loser: 'A' },
-    { match: %w[C Y], w_score: 3, winner: 'C', l_score: 2, loser: 'Y' },
-    { match: %w[B Z], w_score: 3, winner: 'Z', l_score: 2, loser: 'B' }
+    { match: %w[A Z], w_points: 1, winner: 'A', l_points: 3, loser: 'Z' },
+    { match: %w[C X], w_points: 1, winner: 'X', l_points: 3, loser: 'C' },
+    { match: %w[B X], w_points: 2, winner: 'B', l_points: 1, loser: 'X' },
+    { match: %w[A Y], w_points: 2, winner: 'Y', l_points: 1, loser: 'A' },
+    { match: %w[C Y], w_points: 3, winner: 'C', l_points: 2, loser: 'Y' },
+    { match: %w[B Z], w_points: 3, winner: 'Z', l_points: 2, loser: 'B' }
   ].freeze
 
   def results
@@ -38,26 +38,38 @@ class RockPaperScissors
     @match_data
   end
 
-  def match_scores
-    @match_scores = []
+  def match_points
+    @match_points = []
     @match_data.each do |_match|
-      score = 0
-      score = score + 3 + obj_score if tie
-      score = score + 6 + obj_score if winner
-      score += obj_score if !tie && !winner
-      match_scores.push(score)
+      points = 0
+      points += obj_points
+      points += 3 if tie
+      points += 6 if winner
+      match_points.push(points)
     end
-    @match_scores
+    @match_points
   end
 
-  def obj_score
-    POINT_KEY.find { |h| break h[:w_score] if h[:match] == match } if winner
-    TIE_POINT_KEY.find { |h| break h[:score] if h[:match] == match } if tie
-    POINT_KEY.find { |h| break h[:l_score] if h[:match] == match } unless !winner && !tie
+  def obj_points
+    winner_points if winner
+    tie_points if tie
+    loser_points unless !winner && !tie
+  end
+
+  def winner_points
+    POINT_KEY.find { |h| break h[:w_points] if h[:match] == match }
+  end
+
+  def tie_points
+    TIE_POINT_KEY.find { |h| break h[:points] if h[:match] == match }
+  end
+
+  def loser_points
+    POINT_KEY.find { |h| break h[:l_points] if h[:match] == match }
   end
 
   def tie
-    TIE_POINT_KEY.find { |h| break h[:score] if h[:match] == match }
+    TIE_POINT_KEY.find { |h| break h[:points] if h[:match] == match }
   end
 
   def winner
@@ -65,7 +77,7 @@ class RockPaperScissors
   end
 
   def total_score
-    @match_scores.sum
+    @match_points.sum
   end
 end
 
